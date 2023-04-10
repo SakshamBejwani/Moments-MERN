b62e869998137eb3752569cdfcb30734fd4c0592
Saksham Bejwani <sakshambejwani@gmail.com>
2023-04-10 19:08:17 +0530

    v2.0

--- server/index.js
+++ server/index.js
@@ -3,16 +3,21 @@ import bodyParser from 'body-parser';
 import  mongoose  from 'mongoose';
 import cors from 'cors';
 import postRoutes from './routes/posts.js';
+import dotenv from 'dotenv'
 const app = express();
+dotenv.config()
 
 app.use(bodyParser.json({ limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
 app.use(cors())
+
 app.use('/posts', postRoutes)
-const CONNECTION_URL = 'mongodb+srv://saksham:saksham123@cluster0.76oj7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+app.get('/', (req, res) => {
+    res.send('Navigate to /posts for REST API ussage: Saksham Bejwani')
+})
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => app.listen(PORT, () => console.log('Server Running on Port: '+ PORT)))
     .catch((error) => console.log('Error', error.message))
 
